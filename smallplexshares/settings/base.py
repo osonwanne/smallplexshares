b421977fcf0835acfa25e877bccbe949b0439b70
Bon Osonwanne <nosonwan@gmail.com>
2017-07-15 18:49:37 -0700

    added navbar

--- smallplexshares/settings/base.py
+++ smallplexshares/settings/base.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'bootstrap_themes',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
